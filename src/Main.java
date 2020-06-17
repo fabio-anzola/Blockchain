e15c1eed085a09bf5a40961786ab162cf9c9e3e9
tobias <8117@htl.rennweg.at>
2020-06-17 15:36:14 +0200

    Debugging

--- src/Main.java
+++ src/Main.java
@@ -7,20 +7,11 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Blockchain bc = new Blockchain();
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 1; i++) {
-            new Miner(bc).start();
+        new Miner(bc).start();
+        bc.messageCache.add("miner13 sent 10 VC to Bob");
+        bc.messageCache.add("miner13 sent 10 VC to Carshop");
+        while (bc.getSize() < 15) {
         }
-        Thread.sleep(1000);
-        bc.messageCache.add("Tom: Hey, I'm first!");
-        bc.messageCache.add("Sarah: It's not fair!");
-        Thread.sleep(2000);
-        bc.messageCache.add("Sarah: You always will be first because it is your blockchain!");
-        Thread.sleep(1000);
-        bc.messageCache.add("Sarah: Anyway, thank you for this amazing chat.");
-        Thread.sleep(2000);
-        bc.messageCache.add("Tom: You're welcome :)");
-        bc.messageCache.add("Nick: Hey Tom, nice chat");
-
         System.exit(0);
     }
 }
